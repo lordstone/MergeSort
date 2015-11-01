62f2652f93af6d5283bb580e095f7e471a00cfdb
Chengxi (Chen) Shi <chengxis@usc.edu>
2015-11-01 13:27:52 -0800

    Create findmedian.h

---
+++ findmedian.h
@@ -0,0 +1,16 @@
+#ifndef FINDMEDIAN_H
+#define FINDMEDIAN_H
+
+
+class findmedian
+{
+    public:
+        findmedian();
+        void addNum(int);
+        double findMedian();
+        virtual ~findmedian();
+    protected:
+    private:
+};
+
+#endif // FINDMEDIAN_H
